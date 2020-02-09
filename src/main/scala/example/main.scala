66b5f1ece7aad2e2b791e32344b4e7ad7482863c
Junjun Deng <cnjamesdeng@gmail.com>
2020-02-09 18:18:19 +0800

    Support debug form in the docs service

--- src/main/scala/example/main.scala
+++ src/main/scala/example/main.scala
@@ -3,6 +3,7 @@ package example
 import com.example.grpc.hello.Hello
 import com.example.grpc.hello.Hello.HelloReply
 import com.example.grpc.hello.HelloServiceGrpc.HelloServiceImplBase
+import com.linecorp.armeria.common.grpc.GrpcSerializationFormats
 import com.linecorp.armeria.server.Server
 import com.linecorp.armeria.server.docs.DocService
 import com.linecorp.armeria.server.grpc.GrpcService
@@ -16,6 +17,8 @@ object Main extends App {
     .builder()
     .addService(new HelloService())
     .addService(ProtoReflectionService.newInstance())
+    .supportedSerializationFormats(GrpcSerializationFormats.values())
+    .enableUnframedRequests(true)
     .build()
   val server = Server
     .builder()
@@ -36,9 +39,9 @@ class HelloService extends HelloServiceImplBase {
   override def hello(
       request: Hello.HelloRequest,
       responseObserver: StreamObserver[Hello.HelloReply]): Unit = {
-    val message = request.getName
-    logger.info("Received message: {}", message)
-    val reply = HelloReply.newBuilder().setMessage("hi").build()
+    val name = request.getName
+    logger.info("Received message: {}", name)
+    val reply = HelloReply.newBuilder().setMessage(s"How are u, $name").build()
     responseObserver.onNext(reply)
     responseObserver.onCompleted()
   }
